b98bb1c55bb9a39af9975ad2c63c4beaef56c32d
Ömer <omeroguzcelikel@gmail.com>
2022-01-30 10:23:13 +0300

    newFileForP1

---
+++ 01-Project1/01-Project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  01-Project1
+//
+//  Created by Ömer Oğuz Çelikel on 30.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
