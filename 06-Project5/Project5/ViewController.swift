a04b71c3a3cdec041941cfd13556c3c1308a8a5f
Ömer <omeroguzcelikel@gmail.com>
2022-02-06 15:10:31 +0300

    txtUploaded

---
+++ 06-Project5/Project5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by Ömer Oğuz Çelikel on 6.02.2022.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
