ed9d2ac3482eb8a2509ecc3c4fd8beb49b5de53b
Ömer <omeroguzcelikel@gmail.com>
2022-02-08 09:45:09 +0300

    day2

--- 06-Project5/Project5/ViewController.swift
+++ 06-Project5/Project5/ViewController.swift
@@ -5,6 +5,11 @@
 //  Created by Ömer Oğuz Çelikel on 6.02.2022.
 //
 
+/*
+ https://www.hackingwithswift.com/100/28
+ Prepare for submission: lowercased() and IndexPath
+ */
+
 import UIKit
 
 class ViewController: UITableViewController {
@@ -47,6 +52,27 @@ class ViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
         cell.textLabel?.text = usedWords[indexPath.row]
         return cell
+    }
+    
+    @objc func promptForAnswer(){
+        let ac = UIAlertController(title: "Enter Answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        // create submit action
+        let submitAction = UIAlertAction(title: "Submit", style: .default){
+            [weak self, weak ac] _ in
+            guard let answer = ac?.textFields?[0].text else {
+                return
+            }
+            self?.submit(answer)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ answer: String){
+        
     }
 
 
