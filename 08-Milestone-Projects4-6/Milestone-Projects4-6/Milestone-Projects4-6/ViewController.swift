d1b239452c49e89efb634be0ab618eca07efe6df
Ömer <omeroguzcelikel@gmail.com>
2022-07-11 01:19:14 +0300

    shopingListProjectCreated

---
+++ 08-Milestone-Projects4-6/Milestone-Projects4-6/Milestone-Projects4-6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Milestone-Projects4-6
+//
+//  Created by Ömer Oğuz Çelikel on 11.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
