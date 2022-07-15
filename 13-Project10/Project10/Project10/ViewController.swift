bbac51359ebc129aedc6ce7c7caa2ad9a4d6f5f1
Ömer <omeroguzcelikel@gmail.com>
2022-07-15 20:47:34 +0300

    newProject

---
+++ 13-Project10/Project10/Project10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Ömer Oğuz Çelikel on 15.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
