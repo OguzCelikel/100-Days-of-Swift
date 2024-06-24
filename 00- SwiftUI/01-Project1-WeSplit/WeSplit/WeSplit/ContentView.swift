d993b30e1221ae4e4d5771d0b651c9e0a6375ce1
Ömer Oğuz Çelikel <omeroguzcelikel@gmail.com>
2024-06-24 15:02:27 +0300

    ForEEach

--- 00- SwiftUI/01-Project1-WeSplit/WeSplit/WeSplit/ContentView.swift
+++ 00- SwiftUI/01-Project1-WeSplit/WeSplit/WeSplit/ContentView.swift
@@ -10,17 +10,30 @@ import SwiftUI
 struct ContentView: View {
     @State var tapCount = 0
     @State private var name = ""
-
+    
     var body: some View {
-        Form {
-            TextField("Enter your name", text: $name) //read and write
-            Text("Hello, world!")
-            Text(name) // just read
+        NavigationStack {
+            Form {
+                TextField("Enter your name", text: $name) //read and write
+                Text("Hello, world!")
+                Text(name) // just read
+                
+                // both are same
+                ForEach(0..<20) { number in
+                    Text("Row \(number)")
+                }
+                
+                ForEach(0..<20) {
+                    Text("Row \($0)")
+                }
+            }
+            Spacer()
+            Button("Tap Count: \(tapCount)") {
+                tapCount += 1
+            }.padding()
+                .navigationTitle("Test")
+                .navigationBarTitleDisplayMode(.large)
         }
-        Spacer()
-        Button("Tap Count: \(tapCount)") {
-            tapCount += 1
-        }.padding()
     }
 }
 
