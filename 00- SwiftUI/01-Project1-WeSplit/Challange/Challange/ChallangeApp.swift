5fdb41e6d174192b234c0412963661e213c68ba6
Ömer Oğuz Çelikel <omeroguzcelikel@gmail.com>
2024-06-27 16:58:46 +0300

    new project created for challenge

---
+++ 00- SwiftUI/01-Project1-WeSplit/Challange/Challange/ChallangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallangeApp.swift
+//  Challange
+//
+//  Created by Ömer Oğuz Çelikel on 27.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChallangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 00- SwiftUI/01-Project1-WeSplit/Challange/Challange/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Challange
+//
+//  Created by Ömer Oğuz Çelikel on 27.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
